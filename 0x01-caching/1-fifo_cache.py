3a3310481484be991db1dd4c4ec9f85c8148bb78
Jasemonu <jasemonu@gmail.com>
2023-10-24 22:28:52 +0000

    1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""a class FIFOCache that inherits from BaseCaching
+   and is a caching system:
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+        self.queue = []  # FIFO queue to keep track of the order of items
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            """ Check if the number of items in
+            the cache is higher than the maximum
+            """
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Get the first item added to the cache (oldest item)
+                first_item_key = self.queue.pop(0)
+                print(f"DISCARD: {first_item_key}\n")
+                # Remove the oldest item from the cache
+                del self.cache_data[first_item_key]
+
+            # Add the new item to the cache and the FIFO queue
+            self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        if key is not None:
+            return self.cache_data.get(key, None)
+        return None
