6f42d3025cee315c00450af8b2aada02103301ef
Jasemonu <jasemonu@gmail.com>
2023-10-24 22:21:16 +0000

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""a class BasicCache that inherits from BaseCaching and is a caching system:"""
+"""a class BasicCache that inherits from
+BaseCaching and is a caching system:
+
+"""
 
 
 from base_caching import BaseCaching
-
 
 
 class BasicCache(BaseCaching):
@@ -14,13 +16,19 @@ class BasicCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
-        """Check if key or item is None, and don't do anything in that case"""
+        """Check if key or item is None,
+        and don't do anything in that case
+        """
         if key is not None and item is not None:
-            """ Assign the item value to the key in the cache_data dictionary"""
+            """Assign the item value to the key
+            in the cache_data dictionary
+            """
             self.cache_data[key] = item
 
     def get(self, key):
-        """Check if key is None or if the key doesn't exist in the cache_data"""
+        """Check if key is None or if the key doesn't
+        exist in the cache_data
+        """
         if key is not None and key in self.cache_data:
             """ Return the value linked to the key """
             return self.cache_data[key]
