f139fbf2acb9eeeb784912017d9b31174e2d0520
Jasemonu <jasemonu@gmail.com>
2023-10-31 06:09:49 -0700

    1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""set up of basic Flsk app"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+# configure the flask app
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+
+
+@app.route('/')
+def index():
+    """A route declaration"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
