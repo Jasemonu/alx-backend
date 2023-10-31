e957d8288b1dbe120e218245f8dec23d23f07540
Jasemonu <jasemonu@gmail.com>
2023-10-31 06:20:13 -0700

    1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -11,8 +11,11 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-class Config:
+class Config(object):
+    """Config class"""
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 @app.route('/')
