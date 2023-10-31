b0a67cdea3b7f829de2054e3d8f40ac35c2004ed
Jasemonu <jasemonu@gmail.com>
2023-10-31 07:53:30 -0700

    4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -32,7 +32,7 @@ def get_locale():
 @app.route('/')
 def index():
     """A route declaration"""
-    return render_template('1-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
