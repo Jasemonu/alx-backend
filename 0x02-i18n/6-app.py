8099fa8894ee8ef7256dea83b03b12d23f8c8bd7
Jasemonu <jasemonu@gmail.com>
2023-11-01 00:31:02 +0000

    6-app.py

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
-"""set up of basic Flsk app"""
+"""This module sets up a basic Flask app"""
 
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
 
 
 class Config(object):
-    """Config class"""
+    """Creates a Config class with the LANGUAGES attribute"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
-# configure the flask app
-app = Flask(__name__)
-app.config.from_object(Config)
-babel = Babel(app)
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -25,44 +22,43 @@ users = {
 }
 
 
-def get_user():
-    """returns a user dictionary or None if the ID cannot be found
-    """
-    login_id = request.args.get('login_as')
-    if login_id:
-        return users.get(int(login_id))
-    return None
-
-
-@app.before_request
-def before_request() -> None:
-    """_summary_
-    """
-    user = get_user()
-    g.user = user
+@app.route('/')
+def index_page():
+    """This function defines a route for the root URL ('/')"""
+    return render_template('6-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    # Locale from user settings
-    if g.user:
-        locale = g.user.get('locale')
-        if locale and locale in app.config['LANGUAGES']:
+    """Selects a language translation to use for request by order of priority
+    """
+    locales = [
+        request.args.get('locale'),
+        g.user.get('locale', None) if g.user else None,
+        request.accept_languages.best_match(Config.LANGUAGES),
+        Config.BABEL_DEFAULT_LOCALE
+    ]
+
+    for locale in locales:
+        if locale and locale in Config.LANGUAGES:
             return locale
 
-    # ocale from request header
-    locale = request.headers.get('locale', None)
-    if locale in app.config['LANGUAGES']:
-        return locale
 
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+def get_user():
+    """Returns a user dictionary"""
+    user_id = request.args.get('login_as')
+
+    if user_id:
+        try:
+            user_id = int(user_id)
+            if user_id in users:
+                return users[user_id]
+            return None
+        except Exception:
+            return None
 
 
-@app.route('/')
-def index():
-    """A route declaration"""
-    return render_template('6-index.html')
-
-
-if __name__ == '__main__':
-    app.run()
+@app.before_request
+def before_request():
+    """Finds the user"""
+    g.user = get_user()
